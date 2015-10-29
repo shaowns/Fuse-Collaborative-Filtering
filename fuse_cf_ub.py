bc70832d9d67a1d31304e0680ca8149c3085f890
shaowns <shaown@csebuet.org>
2015-10-29 16:22:32 -0400

    Added prioritization to formula recommendations

--- fuse_cf_ub.py
+++ fuse_cf_ub.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from operator import itemgetter
 
 import csv
 import math
@@ -12,7 +13,10 @@ formulas = fl.get_formula_list()
 
 csv_file_fuse = 'fuse_formula_count.csv'
 
-# Set of all vectors from the Fuse corpus, with formula-frequency
+# Set of base formula frequency from the Fuse corpus, with row id for each instance.
+base_ff_set = []
+
+# Set of all vectors from the Fuse corpus, with weighted formula-frequency
 # inverse instance frequency (ff-iif) data for each formula.
 vector_set = []
 
@@ -26,61 +30,61 @@ tuning_factor = 1.0
 testing_instance = []
 
 
-def initialize_formula_imp_weights():
-    for formula in formulas:
-        formula_imp_weight[formula] = 0.0
-
-
-def load_vector_set():
+# Loads the formula frequency from csv into the base set.
+def load_base_ff_set():
     with open(csv_file_fuse, 'r') as f:
         csv_reader = csv.reader(f)
         csv_reader.next()
+
         for row in csv_reader:
             row_data = []
             for element in row:
                 row_data.append(int(element))
-            vector_set.append(row_data)
+            base_ff_set.append(row_data)
 
     # Add the testing instance at the very end.
     test = isv.get_testing_vector('Test.xlsx')
-    vector_set.append(test)
+    base_ff_set.append(test)
 
 
 def calculate_formula_weight():
-    initialize_formula_imp_weights()
-    for instance in vector_set:
+    # Initialize formula weights.
+    for formula in formulas:
+        formula_imp_weight[formula] = 0.0
+
+    for instance in base_ff_set:
         for index in xrange(0, len(formulas)):
             # First get the no. of instances that uses this formula.
             if instance[index] > 0:
                 formula_imp_weight[formulas[index]] += 1
 
     # The weight dictionary has instance count by formula. Now calculate the weight.
-    instances_with_formulas = len(vector_set)
+    instances_with_formulas = len(base_ff_set)
     for key, value in formula_imp_weight.iteritems():
         if value > 0.0:
             value = math.log(instances_with_formulas/value)
             formula_imp_weight[key] = value
 
 
-def calculate_formula_vectors():
-    for i in xrange(0, len(vector_set)):
-        instance = vector_set[i]
+def calculate_weighted_vectors():
+    for instance in base_ff_set:
         instance_total = sum(instance)
+        instance_formula_weights = []
+        for i in xrange(0, len(formulas)):
+            instance_formula_weights.append(tuning_factor * (instance[i]/instance_total) *
+                                            formula_imp_weight[formulas[i]])
 
-        for j in xrange(0, len(formulas)):
-            cur_formula = formulas[j]
-            instance[j] = tuning_factor * (instance[j]/instance_total) * formula_imp_weight[cur_formula]
-
-        vector_set[i] = instance
+        vector_set.append(instance_formula_weights)
 
 
 def get_knn(n):
+    # Separate the testing instance from the vector set.
     global testing_instance
     testing_instance = vector_set.pop()
 
     similarity_results = []
-    for instance in vector_set:
-        similarity_results.append([instance, fcu.cosine_similarity(testing_instance, instance)])
+    for index in xrange(0, len(vector_set)):
+        similarity_results.append([index, fcu.cosine_similarity(testing_instance, vector_set[index])])
 
     # Sort the similarity values, stored in position 1 of results.
     similarity_results = sorted(similarity_results, key=lambda k: k[1], reverse=True)
@@ -88,33 +92,39 @@ def get_knn(n):
     return similarity_results[:n]
 
 
-def get_recommendations(recommendation_instances, n):
-    recommendations = []
-    for i in xrange(0, n):
+def get_recommendations(similars):
+    recommendations = {}
+    input_instance = base_ff_set.pop()
+    for similar in similars:
         # The actual instance to recommend from.
-        instance = recommendation_instances[i][0]
+        similar_instance = base_ff_set[similar[0]]
 
         # Sanity check
-        assert (len(formulas) == len(instance)), "The similar instance doesn't have correct dimension"
-        assert (len(testing_instance) == len(instance)), "The similar instance doesn't have correct dimension"
-
+        assert (len(formulas) == len(similar_instance)), "The similar instance doesn't have correct dimension"
         for index in xrange(0, len(formulas)):
-            if testing_instance[index] == 0.0 and instance[index] > 0.0:
-                recommendations.append(formulas[index])
+            formula = formulas[index]
+            if input_instance[index] == 0 and similar_instance[index] > 0:
+                if formula in recommendations:
+                    recommendations[formula] += similar_instance[index]
+                else:
+                    recommendations[formula] = similar_instance[index]
 
-    return recommendations
+    recommendations_sorted = sorted(recommendations.items(), key=itemgetter(1), reverse=True)
+
+    # Return only the list of formulas.
+    return [item[0] for item in recommendations_sorted]
 
 
 def main():
-    load_vector_set()
+    load_base_ff_set()
 
     calculate_formula_weight()
 
-    calculate_formula_vectors()
+    calculate_weighted_vectors()
 
     results = get_knn(3)
 
-    recommendations = get_recommendations(results, 3)
+    recommendations = get_recommendations(results)
 
     print recommendations
 
